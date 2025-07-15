4f72f9b7991009cc1eac448fc61184092cfcf96d
NNC Organization <106741077+Novo0007@users.noreply.github.com>
2025-07-15 17:37:46 +0530

    Update ProfessionalGameLobby.tsx

--- src/components/games/ProfessionalGameLobby.tsx
+++ src/components/games/ProfessionalGameLobby.tsx
@@ -446,6 +446,8 @@ export const ProfessionalGameLobby: React.FC<ProfessionalGameLobbyProps> = ({
             </Button>
           </div>
         )}
+	      <script async="async" data-cfasync="false" src="//pl27171869.profitableratecpm.com/b34ce61993fe87916059ccdad283666b/invoke.js"></script>
+<div id="container-b34ce61993fe87916059ccdad283666b"></div>
 
         {/* Quick Stats Footer */}
         <div className="mt-12 p-6 bg-white rounded-2xl shadow-sm border border-gray-200">
